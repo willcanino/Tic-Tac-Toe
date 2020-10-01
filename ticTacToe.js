d0bb071c54b89538a959b12ee280000483417701
Will Canino <willcaninoyt@gmail.com>
2020-10-01 16:55:19 +1000

    edited ticTacToe.js

--- ticTacToe.js
+++ ticTacToe.js
@@ -13,10 +13,23 @@ function setup () {
 
 function draw() {
   background(220);
-  for (let i = 0; i < 3; i++){
-    for (let j = 0; i < 3; i++){
+  let w = width / 3;
+  let h = height / 3;
 
+  for (let i = 0; i < 3; i++){
+    for (let j = 0; j < 3; j++){
+      let x = w * i;
+      let y = h * j;
       let spot = board[i][j];
+      textSize(32);
+      if (spot == 'player 1') {
+        noFill();
+        ellipseMode(CORNER);
+        ellipse(x,y,w);
+      } else if *(spot == player2) {
+        line(x,y,x+w,y+h);
+        line(x+w,y,x,y+h);
+      }
       text(spot, x, y);
   }
 }
